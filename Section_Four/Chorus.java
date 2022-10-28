bb76efb28253d8f53c125d4526a34b6316c1ef5f
Bo <hiboss1@gmail.com>
2022-10-28 19:31:28 +0900

    Done for return value

---
+++ Section_Four/Chorus.java
@@ -0,0 +1,17 @@
+public class Chorus{
+    public static void main(String[] args) {
+        Chorus a = new Chorus();
+        a.singChorus();
+        a.singChorus();
+        a.singChorus();
+        a.singChorus();
+        a.singChorus();
+    }
+
+    public void singChorus() {
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonshine");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Blame it on the boogie\n");
+    }
+}
