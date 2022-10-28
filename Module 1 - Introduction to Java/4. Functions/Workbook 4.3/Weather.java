328c4b34a979c74f4bd5aae6467cbad7fa5051d7
Bo <hiboss1@gmail.com>
2022-10-28 19:54:36 +0900

    Done with Workshot4_3

--- Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
+++ Module 1 - Introduction to Java/4. Functions/Workbook 4.3/Weather.java
@@ -6,7 +6,9 @@ public class Weather {
 
         //Task 3 - Call the printTemperatures function. 
         
-        
+        printTemperatures(noon);
+        printTemperatures(evening);
+        printTemperatures(midnight);
     }
 
 
@@ -21,7 +23,10 @@ public class Weather {
      * 1. returns the temperature in celcius. C = (F - 32) * 5/9. 
      */
 
-     
+    public static double fahrenheitToCelsius(double fahrenheit){
+        double C = (fahrenheit - 32) * 5.0 / 9.0;
+        return C;
+    } 
     //Task 2: Make a function here. See the doc comments below. 
 
     /**
@@ -32,4 +37,8 @@ public class Weather {
      *  1. prints: F: <temperature in fahrenheit>.
      *  2. prints: C: <temperature in celsius> . 
      */
+    public static void printTemperatures(double fahrenheit){
+        System.out.println("1. prints: F: " + fahrenheit);
+        System.out.println("2. prints: C: " + fahrenheitToCelsius(fahrenheit));
+    }
 }
