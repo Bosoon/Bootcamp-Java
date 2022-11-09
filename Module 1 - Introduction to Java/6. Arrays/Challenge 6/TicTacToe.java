83ad0297e1a8be46f48990056712a3dc9ba55b3e
Bo <hiboss1@gmail.com>
2022-11-09 14:18:08 +0900

    Done TicTacToe

--- Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -5,63 +5,63 @@ public class TicTacToe {
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
         
+            int[] temp = new int[2];
             System.out.println("\nLet's play tic tac toe");
 
-            //Task 1: Create an array with three rows of '_' characters.
+            char[][] board = new char[3][3];
+            for(int i = 0 ; i < board.length ; i++){
+              for (int j = 0 ; j < board[i].length ; j++){
+                board[i][j] = '_';
+              }
+            }
 
-            //Task 2: Call the function printBoard();
-
-             
-              /*
-              {  Task 3: Loop through turns.
-
-                  if (X) turn {
-                     Task 4: call askUser(). 
-                     Task 5: populate the board using askUser's return value.
-                  } else {
-                      Task 4: call askUser(). 
-                      Task 5: populate the board using askUser's return value. Then, print it.
-
-                  }
-
-                Task 6 - Call the function.
-                   if return value == 3 {
-                     print: X wins and break the loop
-                  } else if return value == -3 {
-                     print: O wins and break the loop
-                  }
-
-              } 
-              */
+            printBoard(board);
+            
+            for(int i = 0 ; i < 9 ; i++){
+              if ( (i % 2) == 0 ){
+                System.out.println("Turn: X");
+                temp = askUser(board);
+                board[temp[0]][temp[1]] = 'X';
+                printBoard(board);
+              } else {
+                System.out.println("Turn: O");
+                temp = askUser(board);
+                board[temp[0]][temp[1]] = 'O';
+                printBoard(board);
+              }
+              if (checkWin(board) == 3){
+                System.out.println("X turn win!");
+                break;
+              } else if (checkWin(board) == -3){
+                System.out.println("O turn win");
+                break;
+              }
+            }
 
             scan.close();
         }
+      
+        public static void printBoard(char[][] board){
+          System.out.println();
+          for (int i = 0 ; i < board.length ; i++){
+            System.out.print("\t");
+            for (int j = 0 ; j < board.length ; j++){
+              System.out.print(board[i][j] + " ");
+            }
+            System.out.println("\n\n");
+          }
+        }
 
-
-    /** Task 2 - Write a function that prints the board.
-     * Function name - printBoard()
-     * @param board (char[][])
-     * 
-     * Inside the function:
-     *   1. print a new line.
-     *   2. print the board.
-     *      • separate each row by two lines. 
-     *      • each row precedes a tab of space
-     *      • each character in the grid has one space from the other character
-     */        
-
-   /** Task 4 - Write a function that lets the user choose a spot
-     * Function name – askUser
-     * @param board (char[][] board)
-     * @return spot (int[])
-     * 
-     * Inside the function
-     *   1. Asks the user: - pick a row and column number: 
-     *   2. Check if the spot is taken. If so, let the user choose again.
-     *   3. Return the row and column in an int[] array.
-     * 
-     */
-
+        public static int[] askUser(char[][] board){
+          while(true){
+            System.out.print("\tpick a row and column number: ");
+            int row = scan.nextInt();
+            int column = scan.nextInt();
+            if (board[row][column] =='_'){
+              return new int[]{row, column};
+            }
+          }
+        }
     /** Task 6 - Write a function that determines the winner
      * Function name - checkWin 
      * @param board (char[][])
@@ -74,6 +74,60 @@ public class TicTacToe {
      *   4. Check the left diagonal for a straight X or straight O (Task 9).
      *   5. Check the right diagonal for a straight X or straight O (Task 10).
      */
+        public static int checkWin(char[][] board){
 
+          int count = 0;
+          for(int i = 0 ; i < board.length; i++){
+            for(int j = 0 ; j < board[i].length; j++){
+              if (board[i][j] == 'X'){
+                count++;
+              }else if (board[i][j] == 'O'){
+                count--;
+              }              
+            }
+            if (count == 3 || count == -3) {
+              return count;
+            }
+            count = 0;
+          }
+
+          count = 0;
+          for(int i = 0; i < board.length ; i++){
+            for(int j = 0; j < board[i].length; j++){
+              if (board[j][i] == 'X'){
+                count++;
+              } else if(board[j][i] == 'O'){
+                count--;
+              }
+            }
+            if (count == 3 || count == -3){
+              return count;
+            }
+            count = 0;
+          }
+
+          count = 0;
+          for(int i = 0 ; i < board.length ; i ++){
+              if (board[i][i] == 'X'){
+                count++;
+              } else if (board[i][i] == 'O') {
+                count--;
+              }
+          }
+          if (count == 3 || count == -3){
+            return count;
+          }
+          count = 0;
+          
+          for(int i = 0 ; i < board.length ; i++){
+            if (board[i][2-i] == 'X'){
+              count++;
+            } else if (board[i][2-i] == 'O') {
+              count--;
+            }
+          }
+
+          return count;
+        }
 
 }
