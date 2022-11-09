4c67b73a2ecfbf4f19b412b307ca03edb676355c
Bo <hiboss1@gmail.com>
2022-11-09 12:04:07 +0900

    Javapedia done

--- Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
+++ Module 1 - Introduction to Java/6. Arrays/Javapedia/Javapedia.java
@@ -7,10 +7,27 @@ public class Javapedia {
         System.out.println("How many historical figures will you register?");
         //Task 1 – Ask the user: how many historical figures will you register?
         //       – Store the value.
-        
+        int numberOfRows = scan.nextInt();
+        scan.nextLine();
         //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
-         
+         String[][] data = new String[numberOfRows][3];
         //Watch out for the nextLine() pitfall. 
+        for (int i =0 ; i < numberOfRows ; i++) {
+
+            System.out.println("\n\tFigure " + (i+1)); 
+
+            System.out.print("\t - Name: ");
+            data[i][0] = scan.nextLine();  
+
+            System.out.print("\t - Date of birth: ");
+            data[i][1] = scan.nextLine();
+
+            System.out.print("\t - Occupation: ");
+            data[i][2] = scan.nextLine(); 
+
+            System.out.print("\n");
+
+        }
         /* Task 3 
         for (____) {
 
@@ -32,9 +49,22 @@ public class Javapedia {
         */
         System.out.println("These are the values you stored:"); 
         //Task 4: call print2DArray. 
+        print2DArray(data);
 
         System.out.print("\nWho do you want information on? ");  
-        
+        String name = scan.nextLine();
+
+        for(int i = 0 ; i < data.length ; i++){
+            if (data[i][0].equals(name)){
+                System.out.println();
+                System.out.println("\tName: " + data[i][0]);
+                System.out.println("\tDate of birth: " + data[i][1]);
+                System.out.println("\tOccupation: " + data[i][2]);
+                break;
+            } else {
+                System.out.println("There is no data for you.");
+            }
+        }
         /*Task 5: Let the user search the database by name. 
             If there's a match:
               print(    tab of space    Name: <name>)
@@ -56,5 +86,14 @@ public class Javapedia {
      *     • each value in database has one space from the other value. 
      *     • print a new line.
      */
+    public static void print2DArray(String[][] array){
+        for(int i = 0 ; i < array.length ; i++){
+            System.out.print("\t");
+            for (int j = 0 ; j < array[i].length ; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.14/RandomNumbers.java
@@ -1,16 +1,23 @@
 public class RandomNumbers {
     public static void main(String[] args) {
         // Task 3 – Create a 2D array with 100 rows and 10 columns.
+        int[][] tdArray = new int[100][10];
+        print2DArray(tdArray);
 
-
+        System.out.println("\nAfter calling randomNumber()\n");
         /** Task 4
          * 
          * 1. Using a nested loop, populate the array with random numbers. 
          * 2. Then, pass the updated array into print2DArray.  
          * 
          */
+        for (int i = 0 ; i < tdArray.length ; i++){
+            for (int j = 0 ; j < tdArray[i].length ; j++){
+                tdArray[i][j] = randomNumber();
+            }
+        }
 
-
+        print2DArray(tdArray);
         
     }
 
@@ -22,7 +29,10 @@ public class RandomNumbers {
      * Inside the function:
      *    1. return a random number between 1 and 100. 
      */
-
+    public static int randomNumber(){
+        int result = (int)(Math.random() * 100) + 1;
+        return result;
+    }
 
     /** Task 2
      * Function name: print2DArray
@@ -34,4 +44,12 @@ public class RandomNumbers {
      *        • Each row takes up one line.
      *        • There's a space between each value.
      */
+    public static void print2DArray(int[][] array){
+        for(int i = 0; i < array.length; i++){
+            for (int j = 0 ; j < array[i].length; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
