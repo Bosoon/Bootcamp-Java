b54bd2d9d548225a5d39a3483e71be4d487f11de
Bo <hiboss1@gmail.com>
2022-11-17 14:46:29 +0900

    done it

--- Section_Seven/Car.java
+++ Section_Seven/Car.java
@@ -1,15 +1,47 @@
 public class Car {
 
-    String maker;
-    double price;
-    int year;
-    String color;
+    private String maker;
+    private double price;
+    private int year;
+    private String color;
 
     public Car(String maker, double price, int year, String color) {
         this.maker = maker;
         this.price = price;
         this.year = year;
         this.color = color;
+    }
+
+    public String getMaker(){
+        return this.maker;
+    }
+
+    public void setMaker(String maker){
+        this.maker = maker;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+    
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setColor(String color){
+        this.color = color ;
     }
 
     public void print(){
--- Section_Seven/Main.java
+++ Section_Seven/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 2020, 5000, "red");
+        Car nissan = new Car("Nissan", 5000, 2018, "red");
         Car dodge = new Car("Dodge", 11000, 2019, "blue");
 
         nissan.print();
         dodge.print();
+
+        System.out.println(nissan.getColor());
+        nissan.setColor("Black Light");
+        nissan.setPrice(nissan.getPrice()/2);
+        nissan.print();
+        
     }
 }
