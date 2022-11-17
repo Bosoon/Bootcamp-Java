7021d760b81a30d96ecbc04f18f6312837b7c4d2
Bo <hiboss1@gmail.com>
2022-11-18 08:49:41 +0900

    Done it

--- Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -4,16 +4,10 @@ public class Main {
   
     public static void main(String[] args) {
 
-        Person bo = new Person();
-        bo.name = "Bo Soon Park";
-        bo.nationality = "South Korea";
-        bo.dateOfBirth = "15/01/76";
-        bo.passport = new String[]{bo.name, bo.nationality, bo.dateOfBirth};
-        bo.seatnumber = 1;
+        Person bo = new Person("Bo Soon Park", "South Korea", "15/01/76");
+        bo.setSeatNumber(1);
       
-        System.out.println(bo.name);
-        System.out.println(bo.nationality);
-        System.out.println(Arrays.toString(bo.passport));
-        System.out.println(bo.seatnumber);
+        bo.print();
+    
     }
 }
--- Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person.java
@@ -1,9 +1,64 @@
 public class Person {
-    String name;
-    String nationality;
-    String dateOfBirth;
-    int seatnumber;
+    private String name;
+    private String nationality;
+    private String dateOfBirth;
+    private int seatNumber;
     
-    String[] passport;
+    private String[] passport = {this.name, this.nationality, this.dateOfBirth};
 
+    public Person(String name, String nationality, String dateOfBirth){
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber){
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setNationality(String nationality){
+        this.nationality = nationality;
+    }
+
+    public String getNationality(){
+        return this.nationality;
+    }
+
+    public void setDateOfBirth(String dateOfBirth){
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDateOfBirth(){
+        return this.dateOfBirth;
+    }
+
+    public void setSeatNumber(int seatNumber){
+        this.seatNumber = seatNumber;
+    }
+
+    public int getSeatNumber(){
+        return this.seatNumber;
+    }
+
+    public String[] getPassport(){
+        return this.passport;
+    }
+
+    public void print(){
+        System.out.println("Name: " + this.getName());
+        System.out.println("Nationality: " + this.getNationality());
+        System.out.println("Date of Birth: " + this.getDateOfBirth());
+        System.out.println("Seat Number: " + this.getSeatNumber());
+    }
 }
